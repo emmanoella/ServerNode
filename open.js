e9a50d744b03624742bf4a3ffa36c3195899487d
emmanoella <40065243+emmanoella@users.noreply.github.com>
2018-06-10 23:45:38 +0000

    Update open.js

--- open.js
+++ open.js
@@ -1,14 +1,8 @@
-//require fait un appel à une bibliothèque de node.js. il existe
-plusieurs bibliothequzes qui peuvent etre telecharger avec NPM ,le
-gestionnaire de paquet Node.js
+//require fait un appel à une bibliothèque de node.js. il existe plusieurs bibliothequzes qui peuvent etre telecharger avec NPM ,le gestionnaire de paquet Node.js
 var http= require ('http');
 var server= http.createServer(function(req, res){
-//renvoyer le code 200 dans l'entete de la reponse qui signifie au
-navigateur "OK" tout va bien
-res.writeHead(200,{"Content-Type":
-"text/html"});//text/css,text/html,text/plain,image/jpeg,video/mp4,application/zip
-ce sont les types de donnée que le serveur devra indiquer qu'il
-apprete à envoyer au client
+//renvoyer le code 200 dans l'entete de la reponse qui signifie aunavigateur "OK" tout va bien
+res.writeHead(200,{"Content-Type":"text/html"}); //text/css,text/html,text/plain,image/jpeg,video/mp4,application/zipce sont les types de donnée que le serveur devra indiquer qu'ilapprete à envoyer au client
 //renvoyer une reponse
 //res.end('Salut tout le monde !');
 //res.end('<p> La page <strong>HTML </strong> !</p>');
